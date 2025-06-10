f3cdfee65f2eafd26fd0cf55a34b06e74beed2e6
Bin4h <111453850+Bin4h@users.noreply.github.com>
2025-06-10 19:47:36 +0400

    Add files via upload

---
+++ Исходники vkr_test/vkr_test.cs
@@ -0,0 +1,247 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+// Запрос пути к файлу
+Console.WriteLine("Введите путь к файлу ");
+string filePath = Console.ReadLine();
+// Считывание файла в виде набора байт
+byte[] fileContent = await File.ReadAllBytesAsync(filePath);
+int reduce = 0;
+
+Console.WriteLine("Введите размер первого среза ");
+
+int part_size_a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите размер второго среза ");
+
+int part_size_b = int.Parse(Console.ReadLine());
+if(part_size_a + part_size_b > fileContent.Length)
+{
+    Console.WriteLine("Размер заданных частей превышает размер файла");
+    Environment.Exit(0);
+}
+int part_size_c = 45; 
+
+if (part_size_a + part_size_b < fileContent.Length)
+{
+    part_size_c += (int)Math.Ceiling((fileContent.Length - (part_size_a + part_size_b) * 2) / 2.0);
+}
+
+if ((fileContent.Length - (part_size_a + part_size_b) * 2) % 2 != 0)
+{
+    reduce = 2 - fileContent.Length % 2;
+}
+string extension = Path.GetExtension(filePath);
+string combined = part_size_a.ToString() + "!" + part_size_b.ToString() + "!" + part_size_c.ToString() + "!" + extension + "!" + reduce;
+// Сохранение информации в виде набора байт
+byte[] byteArray = Encoding.UTF8.GetBytes(combined);
+int j = 0;
+// Представление файла в виде массивов байт
+byte[] data_1 = new byte[part_size_a];
+for (int i = 0; i < data_1.Length; i++)
+{
+    if (j < fileContent.Length)
+    {
+        data_1[i] = fileContent[j];
+        j++;
+    }
+    else
+    {
+        data_1[i] = 0;
+        j++;
+    }
+}
+
+byte[] data_2 = new byte[part_size_a];
+for (int i = 0; i < data_2.Length; i++)
+{
+    if (j < fileContent.Length)
+    {
+        data_2[i] = fileContent[j];
+        j++;
+    }
+    else
+    {
+        data_2[i] = 0;
+        j++;
+    }
+}
+
+byte[] data_3 = new byte[part_size_b];
+for (int i = 0; i < data_3.Length; i++)
+{
+    if (j < fileContent.Length)
+    {
+        data_3[i] = fileContent[j];
+        j++;
+    }
+    else
+    {
+        data_3[i] = 0;
+        j++;
+    }
+}
+
+byte[] data_4 = new byte[part_size_b];
+for (int i = 0; i < data_4.Length; i++)
+{
+    if (j < fileContent.Length)
+    {
+        data_4[i] = fileContent[j];
+        j++;
+    }
+    else
+    {
+        data_4[i] = 0;
+        j++;
+    }
+}
+
+byte[] data_5 = new byte[part_size_c - 45];
+for (int i = 0; i < data_5.Length; i++)
+{
+    if (j < fileContent.Length)
+    {
+        data_5[i] = fileContent[j];
+        j++;
+    }
+    else
+    {
+        data_5[i] = 0;
+        j++;
+    }
+}
+
+byte[] data_6 = new byte[part_size_c - 45];
+
+for (int i = 0; i < data_6.Length; i++)
+{
+    if (j < fileContent.Length)
+    {
+        data_6[i] = fileContent[j];
+        j++;
+    }
+    else
+    {
+        data_6[i] = 0;
+        j++;
+    }
+}
+// Расчет массивов четности
+// И заполнение итоговых файлов
+
+byte[] data_1_final = new byte[part_size_a + part_size_b + part_size_c];
+j = 0;
+
+for (int i = 0; i < data_1.Length; i++)
+{
+    data_1_final[j] = data_1[i];
+    j++;
+}
+
+for (int i = 0; i < data_3.Length; i++)
+{
+    data_1_final[j] = data_3[i];
+    j++;
+}
+
+byte[] data_56_parity = SolveParity(data_5, data_6);
+
+for (int i = 0; i < data_56_parity.Length; i++)
+{
+    data_1_final[j] = data_56_parity[i];
+    j++;
+}
+
+for (int i = 0; i < byteArray.Length; i++)
+{
+    data_1_final[j] = byteArray[i];
+    j++;
+}
+
+byte[] data_2_final = new byte[part_size_a + part_size_b + part_size_c];
+j = 0;
+
+for (int i = 0; i < data_2.Length; i++)
+{
+    data_2_final[j] = data_2[i];
+    j++;
+}
+
+for (int i = 0; i < data_5.Length; i++)
+{
+    data_2_final[j] = data_5[i];
+    j++;
+}
+
+byte[] data_34_parity = SolveParity(data_3, data_4);
+
+for (int i = 0; i < data_34_parity.Length; i++)
+{
+    data_2_final[j] = data_34_parity[i];
+    j++;
+}
+
+for (int i = 0; i < byteArray.Length; i++)
+{
+    data_2_final[j] = byteArray[i];
+    j++;
+}
+
+byte[] data_3_final = new byte[part_size_a + part_size_b + part_size_c];
+j = 0;
+
+for (int i = 0; i < data_4.Length; i++)
+{
+    data_3_final[j] = data_4[i];
+    j++;
+}
+
+for (int i = 0; i < data_6.Length; i++)
+{
+    data_3_final[j] = data_6[i];
+    j++;
+}
+
+byte[] data_12_parity = SolveParity(data_1, data_2);
+
+for (int i = 0; i < data_12_parity.Length; i++)
+{
+    data_3_final[j] = data_12_parity[i];
+    j++;
+}
+
+for (int i = 0; i < byteArray.Length; i++)
+{
+    data_3_final[j] = byteArray[i];
+    j++;
+}
+
+string dir = Directory.GetCurrentDirectory();
+
+// Сохранение разделенных файлов
+
+string fileName = Path.GetFileNameWithoutExtension(filePath);
+string cur_fileName = fileName + "_1";
+
+await File.WriteAllBytesAsync(Path.Combine(dir, cur_fileName), data_1_final);
+
+cur_fileName = fileName + "_2";
+
+await File.WriteAllBytesAsync(Path.Combine(dir, cur_fileName), data_2_final);
+
+cur_fileName = fileName + "_3";
+
+await File.WriteAllBytesAsync(Path.Combine(dir, cur_fileName), data_3_final);
+
+Console.WriteLine("Файлы успешно представлены в виде набора байтов, они схоранены в виде файлов");
+Console.ReadLine();
+
+byte[] SolveParity(byte[] data_1, byte[] data_2)
+{
+    byte[] parity = new byte[data_1.Length];
+    for (int i = 0; i < parity.Length; i++)
+        parity[i] = (byte)(data_1[i] ^ data_2[i]);
+    return parity;
+}
+
---
+++ Исходники vkr_test/vkr_test_verification.cs
@@ -0,0 +1,495 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+// Получение папки с хранимыми частями
+
+string cur_fileName;
+string cur_filepath;
+string response1 = "2";
+string response2 = "2";
+string response3 = "2";
+string file1Path = "";
+string file2Path = "";
+string file3Path = "";
+Console.WriteLine("Есть ли доступ к 1 файлу?(1.Да 2.Нет)");
+response1 = Console.ReadLine();
+if (response1 == "1")
+{
+    Console.WriteLine("Введите путь к файлу 1");
+    file1Path = Console.ReadLine();
+}
+Console.WriteLine("Есть ли доступ к 2 файлу?(1.Да 2.Нет)");
+response2 = Console.ReadLine();
+if (response2 == "1")
+{
+    Console.WriteLine("Введите путь к файлу 2");
+    file2Path = Console.ReadLine();
+}
+Console.WriteLine("Есть ли доступ к 3 файлу?(1.Да 2.Нет)");
+response3 = Console.ReadLine();
+if (response3 == "1")
+{
+    Console.WriteLine("Введите путь к файлу 3");
+    file3Path = Console.ReadLine();
+}
+
+// Проверка файлов на отсутствие
+if (response1 == "2")
+{
+    // Завершение программы если не хватает больше одного файла
+    if (response2 == "2" || response3 == "2")
+    {
+        Console.WriteLine("Больше одного файла утеряно");
+        Console.ReadLine();
+        Environment.Exit(0);
+    }
+    Console.WriteLine("Утерян диск 1, производится восстановление");
+    //Подгрузка данных
+
+    byte[] data_2_final = await File.ReadAllBytesAsync(file2Path);
+
+    byte[] data_3_final = await File.ReadAllBytesAsync(file3Path);
+
+    byte[] reduce_bytes = new byte[45];
+    int j = 0;
+    for(int i = data_2_final.Length - 45; i < data_2_final.Length; i++)
+    {
+        reduce_bytes[j] = data_2_final[i];
+        j++;
+    }
+    string combinedString = Encoding.UTF8.GetString(reduce_bytes).Replace("\0", "");
+    string[] parts = combinedString.Split('!');
+    int part_size_a = int.Parse(parts[0]);
+    int part_size_b = int.Parse(parts[1]);
+    int part_size_c = int.Parse(parts[2]);
+    string extention = parts[3];
+    int reduce = int.Parse(parts[4]);
+
+    // Собрка файла с восстановлнием
+    byte[] data = new byte[(part_size_a + part_size_b + part_size_c - 45) * 2 - reduce];
+    j = 0;
+    byte[] data_2 = new byte[part_size_a];
+    for (int i = 0; i < data_2.Length; i++)
+    {
+        data_2[i] = data_2_final[j];
+        j++;
+    }
+    byte[] data_5 = new byte[part_size_c - 45];
+    for (int i = 0; i < part_size_c - 45; i++)
+    {
+        data_5[i] = data_2_final[j];
+        j++;
+    }
+    byte[] data_34_parity = new byte[part_size_b];
+    for (int i = 0; i < part_size_b; i++)
+    {
+        data_34_parity[i] = data_2_final[j];
+        j++;
+    }
+
+    j = 0;
+    byte[] data_4 = new byte[part_size_b];
+    for (int i = 0; i < part_size_b; i++)
+    {
+        data_4[i] = data_3_final[j];
+        j++;
+    }
+    byte[] data_6 = new byte[part_size_c - 45];
+    for (int i = 0; i < part_size_c - 45; i++)
+    {
+        data_6[i] = data_3_final[j];
+        j++;
+    }
+    byte[] data_12_parity = new byte[part_size_a];
+    for (int i = 0; i < part_size_a; i++)
+    {
+        data_12_parity[i] = data_3_final[j];
+        j++;
+    }
+
+    byte[] data_1 = SolveParity(data_2, data_12_parity);
+    byte[] data_3 = SolveParity(data_4, data_34_parity);
+
+    j = 0;
+    for (int i = 0; i < data_1.Length; i++)
+    {
+        data[j] = data_1[i];
+        j++;
+    }
+    for (int i = 0; i < data_2.Length; i++)
+    {
+        data[j] = data_2[i];
+        j++;
+    }
+    for (int i = 0; i < data_3.Length; i++)
+    {
+        data[j] = data_3[i];
+        j++;
+    }
+    for (int i = 0; i < data_4.Length; i++)
+    {
+        data[j] = data_4[i];
+        j++;
+    }
+    for (int i = 0; i < data_5.Length; i++)
+    {
+        data[j] = data_5[i];
+        j++;
+    }
+    for (int i = 0; i < data_6.Length - reduce; i++)
+    {
+        data[j] = data_6[i];
+        j++;
+    }
+
+    Console.WriteLine("Введите название файла для сохранения");
+    cur_fileName = Console.ReadLine() + extention;
+    Console.WriteLine("Введите путь для сохранения файла");
+    string path = Console.ReadLine();
+    await File.WriteAllBytesAsync(Path.Combine(path, cur_fileName), data);
+    Console.WriteLine("Файл собран с восстановления");
+} 
+else if(response2 == "2")
+{
+    // Завершение программы если не хватает больше одного файла
+    if (response1 == "2" || response3 == "2")
+    {
+        Console.WriteLine("Больше одного файла утеряно");
+        Console.ReadLine();
+        Environment.Exit(0);
+    }
+    Console.WriteLine("Утерян диск _2, производится восстановление");
+    // Подгрузка данных
+    byte[] data_1_final = await File.ReadAllBytesAsync(file1Path);
+
+    byte[] data_3_final = await File.ReadAllBytesAsync(file3Path);
+
+    byte[] reduce_bytes = new byte[45];
+    int j = 0;
+    for (int i = data_1_final.Length - 45; i < data_1_final.Length; i++)
+    {
+        reduce_bytes[j] = data_1_final[i];
+        j++;
+    }
+    string combinedString = Encoding.UTF8.GetString(reduce_bytes).Replace("\0", "");
+    string[] parts = combinedString.Split('!');
+    int part_size_a = int.Parse(parts[0]);
+    int part_size_b = int.Parse(parts[1]);
+    int part_size_c = int.Parse(parts[2]);
+    string extention = parts[3];
+    int reduce = int.Parse(parts[4]);
+
+    // Восстановление файла
+    byte[] data = new byte[(part_size_a + part_size_b + part_size_c - 45) * 2 - reduce];
+    j = 0;
+    byte[] data_1 = new byte[part_size_a];
+    for (int i = 0; i < part_size_a; i++)
+    {
+        data_1[i] = data_1_final[j];
+        j++;
+    }
+    byte[] data_3 = new byte[part_size_b];
+    for (int i = 0; i < part_size_b; i++)
+    {
+        data_3[i] = data_1_final[j];
+        j++;
+    }
+    byte[] data_56_parity = new byte[part_size_c - 45];
+    for (int i = 0; i < part_size_c - 45; i++)
+    {
+        data_56_parity[i] = data_1_final[j];
+        j++;
+    }
+
+    j = 0;
+    byte[] data_4 = new byte[part_size_b];
+    for (int i = 0; i < part_size_b; i++)
+    {
+        data_4[i] = data_3_final[j];
+        j++;
+    }
+    byte[] data_6 = new byte[part_size_c - 45];
+    for (int i = 0; i < part_size_c - 45; i++)
+    {
+        data_6[i] = data_3_final[j];
+        j++;
+    }
+    byte[] data_12_parity = new byte[part_size_a];
+    for (int i = 0; i < part_size_a; i++)
+    {
+        data_12_parity[i] = data_3_final[j];
+        j++;
+    }
+
+    byte[] data_2 = SolveParity(data_1, data_12_parity);
+    byte[] data_5 = SolveParity(data_6, data_56_parity);
+
+    j = 0;
+    for (int i = 0; i < data_1.Length; i++)
+    {
+        data[j] = data_1[i];
+        j++;
+    }
+    for (int i = 0; i < data_2.Length; i++)
+    {
+        data[j] = data_2[i];
+        j++;
+    }
+    for (int i = 0; i < data_3.Length; i++)
+    {
+        data[j] = data_3[i];
+        j++;
+    }
+    for (int i = 0; i < data_4.Length; i++)
+    {
+        data[j] = data_4[i];
+        j++;
+    }
+    for (int i = 0; i < data_5.Length; i++)
+    {
+        data[j] = data_5[i];
+        j++;
+    }
+    for (int i = 0; i < data_6.Length - reduce; i++)
+    {
+        data[j] = data_6[i];
+        j++;
+    }
+    Console.WriteLine("Введите название файла для сохранения");
+    cur_fileName = Console.ReadLine() + extention;
+    Console.WriteLine("Введите путь для сохранения файла");
+    string path = Console.ReadLine();
+    await File.WriteAllBytesAsync(Path.Combine(path, cur_fileName), data);
+    Console.WriteLine("Файл собран без восстановления");
+
+} else if(response3 == "2")
+{
+    // Завершение программы если не хватает больше одного файла
+    if (response1 == "2" || response2 == "2")
+    {
+        Console.WriteLine("Больше одного файла утеряно");
+        Console.ReadLine();
+        Environment.Exit(0);
+    }
+    Console.WriteLine("Утерян диск _3, производится восстановление");
+    // Подгрузка данных
+    byte[] data_1_final = await File.ReadAllBytesAsync(file1Path);
+
+    byte[] data_2_final = await File.ReadAllBytesAsync(file2Path);
+
+    byte[] reduce_bytes = new byte[45];
+    int j = 0;
+    for (int i = data_1_final.Length - 45; i < data_1_final.Length; i++)
+    {
+        reduce_bytes[j] = data_1_final[i];
+        j++;
+    }
+    string combinedString = Encoding.UTF8.GetString(reduce_bytes).Replace("\0", "");
+    string[] parts = combinedString.Split('!');
+    int part_size_a = int.Parse(parts[0]);
+    int part_size_b = int.Parse(parts[1]);
+    int part_size_c = int.Parse(parts[2]);
+    string extention = parts[3];
+    int reduce = int.Parse(parts[4]);
+
+    // Восстановление файла
+    byte[] data = new byte[(part_size_a + part_size_b + part_size_c - 45) * 2 - reduce];
+    j = 0;
+    byte[] data_1 = new byte[part_size_a];
+    for (int i = 0; i < part_size_a; i++)
+    {
+        data_1[i] = data_1_final[j];
+        j++;
+    }
+    byte[] data_3 = new byte[part_size_b];
+    for (int i = 0; i < part_size_b; i++)
+    {
+        data_3[i] = data_1_final[j];
+        j++;
+    }
+    byte[] data_56_parity = new byte[part_size_c - 45];
+    for (int i = 0; i < part_size_c - 45; i++)
+    {
+        data_56_parity[i] = data_1_final[j];
+        j++;
+    }
+
+    j = 0;
+    byte[] data_2 = new byte[part_size_a];
+    for (int i = 0; i < part_size_a; i++)
+    {
+        data_2[i] = data_2_final[j];
+        j++;
+    }
+    byte[] data_5 = new byte[part_size_c - 45];
+    for (int i = 0; i < part_size_c - 45; i++)
+    {
+        data_5[i] = data_2_final[j];
+        j++;
+    }
+    byte[] data_34_parity = new byte[part_size_b];
+    for (int i = 0; i < part_size_b; i++)
+    {
+        data_34_parity[i] = data_2_final[j];
+        j++;
+    }
+
+    byte[] data_4 = SolveParity(data_3, data_34_parity);
+    byte[] data_6 = SolveParity(data_5, data_56_parity);
+
+    j = 0;
+    for (int i = 0; i < data_1.Length; i++)
+    {
+        data[j] = data_1[i];
+        j++;
+    }
+    for (int i = 0; i < data_2.Length; i++)
+    {
+        data[j] = data_2[i];
+        j++;
+    }
+    for (int i = 0; i < data_3.Length; i++)
+    {
+        data[j] = data_3[i];
+        j++;
+    }
+    for (int i = 0; i < data_4.Length; i++)
+    {
+        data[j] = data_4[i];
+        j++;
+    }
+    for (int i = 0; i < data_5.Length; i++)
+    {
+        data[j] = data_5[i];
+        j++;
+    }
+    for (int i = 0; i < data_6.Length - reduce; i++)
+    {
+        data[j] = data_6[i];
+        j++;
+    }
+    Console.WriteLine("Введите название файла для сохранения");
+    cur_fileName = Console.ReadLine() + extention;
+    Console.WriteLine("Введите путь для сохранения файла");
+    string path = Console.ReadLine();
+    await File.WriteAllBytesAsync(Path.Combine(path, cur_fileName), data);
+    Console.WriteLine("Файл собран без восстановления");
+
+} else if (response1 == "1" && response2 == "1" && response3 == "1")
+{
+    Console.WriteLine("Все файлы на месте, производится сборка ");
+    // Подгрузка данных
+    byte[] data_1_final = await File.ReadAllBytesAsync(file1Path);
+
+    byte[] data_2_final = await File.ReadAllBytesAsync(file2Path);
+
+    byte[] data_3_final = await File.ReadAllBytesAsync(file3Path);
+
+    byte[] reduce_bytes = new byte[45];
+    int j = 0;
+    for (int i = data_1_final.Length - 45; i < data_1_final.Length; i++)
+    {
+        reduce_bytes[j] = data_1_final[i];
+        j++;
+    }
+    string combinedString = Encoding.UTF8.GetString(reduce_bytes).Replace("\0", "");
+    string[] parts = combinedString.Split('!');
+    int part_size_a = int.Parse(parts[0]);
+    int part_size_b = int.Parse(parts[1]);
+    int part_size_c = int.Parse(parts[2]);
+    string extention = parts[3];
+    int reduce = int.Parse(parts[4]);
+
+    byte[] data = new byte[(part_size_a + part_size_b + part_size_c - 45) * 2 - reduce];
+    // Сборка файла из частей
+    j = 0;
+    byte[] data_1 = new byte[part_size_a];
+    for (int i = 0; i < part_size_a; i++)
+    {
+        data_1[i] = data_1_final[j];
+        j++;
+    }
+    byte[] data_3 = new byte[part_size_b];
+    for (int i = 0; i < part_size_b; i++)
+    {
+        data_3[i] = data_1_final[j];
+        j++;
+    }
+    j = 0;
+    byte[] data_2 = new byte[part_size_a];
+    for (int i = 0; i < part_size_a; i++)
+    {
+        data_2[i] = data_2_final[j];
+        j++;
+    }
+    byte[] data_5 = new byte[part_size_c - 45];
+    for (int i = 0; i < part_size_c - 45; i++)
+    {
+        data_5[i] = data_2_final[j];
+        j++;
+    }
+    j = 0;
+    byte[] data_4 = new byte[part_size_b];
+    for (int i = 0; i < part_size_b; i++)
+    {
+        data_4[i] = data_3_final[j];
+        j++;
+    }
+    byte[] data_6 = new byte[part_size_c - 45];
+    for (int i = 0; i < part_size_c - 45; i++)
+    {
+        data_6[i] = data_3_final[j];
+        j++;
+    }
+    j = 0;
+
+    for(int i = 0; i < data_1.Length; i++)
+    {
+        data[j] = data_1[i];
+        j++;
+    }
+    for (int i = 0; i < data_2.Length; i++)
+    {
+        data[j] = data_2[i];
+        j++;
+    }
+    for (int i = 0; i < data_3.Length; i++)
+    {
+        data[j] = data_3[i];
+        j++;
+    }
+    for (int i = 0; i < data_4.Length; i++)
+    {
+        data[j] = data_4[i];
+        j++;
+    }
+    for (int i = 0; i < data_5.Length; i++)
+    {
+        data[j] = data_5[i];
+        j++;
+    }
+    for (int i = 0; i < data_6.Length - reduce; i++)
+    {
+        data[j] = data_6[i];
+        j++;
+    }
+    Console.WriteLine("Введите название файла для сохранения");
+    cur_fileName = Console.ReadLine() + extention;
+    Console.WriteLine("Введите путь для сохранения файла");
+    string path = Console.ReadLine();
+    await File.WriteAllBytesAsync(Path.Combine(path, cur_fileName), data);
+    Console.WriteLine("Файл собран без восстановления");
+}
+
+Console.WriteLine("Работа программы завершена");
+Console.ReadLine();
+
+byte[] SolveParity(byte[] data_1, byte[] data_2)
+{
+    byte[] parity = new byte[data_1.Length];
+    for (int i = 0; i < parity.Length; i++)
+        parity[i] = (byte)(data_1[i] ^ data_2[i]);
+    return parity;
+}
